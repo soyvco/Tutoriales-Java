37491fc1e74f42810fbf0564c4f47593daad26c2
vcoromero <vcoromero@lnx>
2015-06-17 14:56:53 -0500

    Primeros ajustes para la caja registradora.

---
+++ Tutorial - Caja Registradora/MCajaRegistradora.java
@@ -0,0 +1,11 @@
+
+public class MCajaRegistradora
+{
+   public static void main(String[] args)
+   {
+      GUICajaRegistradora cr=new GUICajaRegistradora();
+      cr.setVisible(true);
+      cr.repaint();
+      cr.doLayout();
+   }
+}
